85463c8d862ad8459501139d8dc0b623ca74d861
aviral-dot <aviralbharti832002@gmail.com>
2026-09-07 19:54:02 +0530

    api changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 """Production-style Streamlit frontend for RAGFury."""
 
+import os
 import time
 
 import requests
 import streamlit as st
 
-API_URL = "http://127.0.0.1:8000"
+API_URL = os.getenv(
+    "RAGFURY_API_URL",
+    "http://127.0.0.1:8000",
+)
 
 
 # ============================================================
